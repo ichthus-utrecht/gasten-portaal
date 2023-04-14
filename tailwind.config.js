9570e3b3f6b98702682b06cb441950016379482a
BlockCat <zinoonomiwo@gmail.com>
2023-04-14 11:25:01 +0200

    work on header and nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/*.html", "./static/css/**/*.css"],
+  content: ["./layouts/**/*.html", "./static/css/**/*.css", "./config.yml"],
   theme: {
     extend: {
       fontFamily: {
         'page-head': ['"Bree Serif"', 'serif'],
       },
+      colors: {
+        'ichthus-blue': '#1e2258',
+        'ichthus-red': '#980449'
+      },
       textColor: {
-        'ichthus-blue': '#1e2258'
+        'ichthus-blue': '#1e2258',
+        'ichthus-red': '#980449'
       }
     },
   },
